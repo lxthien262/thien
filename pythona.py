0062159cd4be23fa038a67d6057deefd32b0e112
lxthien262 <18h1040057@ut.edu.vn>
2021-03-09 22:28:22 +0700

    first commit

--- pythona.py
+++ pythona.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-
 """
-
-Created on Sat Mar  6 10:52:12 2021
+Created on Tue Mar  9 22:09:30 2021
 
 @author: DELL
 """
 
-from file_thutu1 import thutu
 cot=int(input("so can nhap la :"))
 matrix = []
 print("nhap so  :")
@@ -15,10 +12,7 @@ for i in range(1):
     a=[]
     for j in range(cot):
         a.append(int(input()))
-    thutu(a)
     matrix.append(a)
 for i in range (1):
     for j in range(cot):
         print(matrix[i][j], end = "")
-    
-        
